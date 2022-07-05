6e038e5cf75d5232a59b1647d266eb91a412edc9
Damian Niewiadomski <fjorden@outlook.com>
2022-07-05 16:37:37 +0200

    first commit

---
+++ SubTxtToSrt.py
@@ -0,0 +1,33 @@
+from cgitb import text
+import re
+import time
+
+with open('Tai chi zero.txt', 'r', encoding="utf8") as file:
+    txt = file.read()
+
+txt = txt.splitlines()
+fps = float(txt[0].strip('{1}'))
+txt.pop(0)
+
+srt = []
+frase_number = 1
+
+for line in txt:
+    x = re.findall('((?={).+?(?<=}))((?={).+?(?<=}))(.+)', line)
+    start = time.strftime('%H:%M:%S', time.gmtime(int(x[0][0].strip('{}'))//fps))
+    miliseconds = str(int(x[0][0].strip('{}'))%fps/fps)[2:5]
+    start = f'{start},{miliseconds}'
+    stop = time.strftime('%H:%M:%S', time.gmtime(int(x[0][1].strip('{}'))//fps))
+    miliseconds = str(int(x[0][1].strip('{}'))%fps/fps)[2:5]
+    stop = f'{stop},{miliseconds}'
+    text = str(x[0][2]).replace('|',' ')
+    frase = f'{frase_number}\n{start} --> {stop}\n{text}\n\n'
+    srt.append(frase)
+    frase_number += 1
+
+ready_srt = ''.join(srt)
+
+
+
+with open('Tai chi zero.srt', 'w', encoding='utf8') as file:
+    file.write(ready_srt)
